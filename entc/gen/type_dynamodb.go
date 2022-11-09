e2cf027029721811a5cd600d15e18fd891dae9fa
Sinh Ngo <kaka.ngo@gmail.com>
2022-11-09 14:46:57 +0700

    BE-2281: Update DynamoDB template for M2M query

--- entc/gen/type_dynamodb.go
+++ entc/gen/type_dynamodb.go
@@ -1,6 +1,10 @@
 package gen
 
-import dyschema "entgo.io/ent/dialect/dynamodb/schema"
+import (
+	"fmt"
+
+	dyschema "entgo.io/ent/dialect/dynamodb/schema"
+)
 
 // DyAttribute converts template Field to DynamoDB attribute.
 func (f Field) DyAttribute() *dyschema.Attribute {
@@ -15,3 +19,21 @@ func (e Edge) AttributeConstant() string { return pascal(e.Name) + "Attribute" }
 
 // AttributesConstant returns the attribute name of the relation attributes. Used for M2M edges.
 func (e Edge) AttributesConstant() string { return pascal(e.Name) + "Attributes" }
+
+// Key returns the relation key of "from" table. Used for M2M edges.
+func (e Edge) Key() string {
+	if e.IsInverse() {
+		return e.Rel.Keys()[0]
+	}
+
+	return e.Rel.Keys()[1]
+}
+
+// Keys returns the relation key in the relation tables.
+func (r Relation) Keys() []string {
+	if len(r.Columns) == 0 {
+		panic(fmt.Sprintf("missing keys for Relation.Table: %s", r.Table))
+	}
+
+	return r.Columns
+}
